d25cf65bbe85362aa6591fa00f97b6526bc9b949
Elly Diallo <diallops01@gmail.com>
2021-06-02 14:58:07 +0200

    server ok + fetch issue

--- js/index.js
+++ js/index.js
@@ -1,28 +1,66 @@
-/*const responseApi = fetch("http://localhost:3000/api/teddies",{method:GET})
-.then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    console.log(value);
-  })
-  .catch(function(err) {
-    console.log("une erreur"+ err +"est survenue");
-  });*/
+const products =  fetch("http://localhost:3000/api/teddies" )
+.then(function(response) {
+  response.json()
+    .then(function(response) {
+      alert(response);
+    })
+})
+
+
+/*fetch("http://localhost:3000/api/teddies")
+
+.then(function(response) {
+  alert(response.json);
+})
+
+
+.catch(error => alert("Erreur :" + error));*/
 
 
 
 
- /* function addProduct(responseApi){
-      for
-  }*/
 
-
- let responseApi = new XMLHttpRequest();
+/*const responseApi = new XMLHttpRequest();
 
   responseApi.open("GET","http://localhost:3000/api/teddies");
 
   responseApi.responseType = "json";
 
   responseApi.send();
+
+  responseApi.onload = function(){
+    if(responseApi.status != 200){
+      alert("Erreur " + responseApi.status + " : " + responseApi.statusText);
+    
+  }else{
+    
+    alert(responseApi.response.length + " octets  téléchargés\n" + JSON.stringify(responseApi.response));
+    JSON.stringify(responseApi.response);
+  }
+};
+
+responseApi.onerror = function(){
+  alert("La réponse a échoué.");
+};
+
+responseApi.onprogress = function(event){
+  if(event.lengthComputable){
+    alert(event.loaded + " octets reçus sur un total de " + event.total);
+  }
+};
+
+console.log(responseApi);*/
+
+/*function createCard (responseApi){
+
+  const div = document.createElement("div");
+  div.innerHTML = responseApi.name;
+  div.setAttribute(
+    "class","col-10 col-lg-5"
+  );
+
+  const image = document.createElement("img");
+  image.innerHTML = responseApi.imageUrl;
+  image.setAttribute("width","100%");
+
+};*/
